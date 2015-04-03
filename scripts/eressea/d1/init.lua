94c6d8d44bd03056a9d72820d87fc1836875e27a
Don <eresseahl@gmail.com>
2015-04-03 10:29:13 -0400

    Addded d1 ruleset
    Ruleset based off of E2

---
+++ scripts/eressea/d1/init.lua
@@ -0,0 +1,9 @@
+require 'eressea.spells'
+eressea.log.debug("rules for game D1")
+
+return {
+    require('eressea'),
+    require('eressea.artint'),
+    require('eressea.astral'),
+    require('eressea.ents')
+}
